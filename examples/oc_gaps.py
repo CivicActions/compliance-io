be543d00a2451f87ac5050a7de4b4f148973d208
Fen Labalme <fen@civicactions.com>
2022-03-16 21:57:35 -0400

    ISPGBSS-852 gap analysis with ARS 3.1

--- examples/oc_gaps.py
+++ examples/oc_gaps.py
@@ -3,17 +3,20 @@ from pathlib import Path
 
 import click
 import json
+import yaml
 import requests
 
 from complianceio import opencontrol
+from complianceio.oscal.oscal import oscalize_control_id
 
 """
-Read an OpenControl repo and perform gap analysis against a Catalog.
-TODO: apply a Profile to the catalog to tailor out e.g. High controls.
+Read an OpenControl repo and perform gap analysis against a Catalog Baseline.
 Return:
     Count and List of Controls Fully Inherited
     Count and list of Controls with Shared Responsibility
     Count and list of Controls have no implementation statement
+TODO:
+    Hybrid should trump Inherited, but shared should not
 """
 
 
@@ -77,52 +80,75 @@ def read_opencontrol_components(source, profile_ids):
 
     p = Path(source)
     oc = opencontrol.load(p)
-    for comp in oc.components:
-        print("{:s} - {:s}".format(comp.key, comp.name))
 
+    for comp in oc.components:
         sorted_controls = sorted(comp.satisfies, key=lambda c: c.standard_key)
         grouped_controls = groupby(sorted_controls, lambda c: c.standard_key)
 
-        for standard_key, controls in grouped_controls:
-            if standard_key not in catalog_keys:
-                catalog_keys[standard_key] = get_catalog_keys(standard_key, profile_ids)
-            print(standard_key)
+        for standard, controls in grouped_controls:
+            if standard not in catalog_keys:
+                catalog_keys[standard] = get_catalog_keys(standard, profile_ids)
 
             for control in controls:
                 key = control.control_key
-                status = "Implemented"
                 if (
                         control.parameters and
                         control.parameters[0].key == "security_control_type"
                 ):
-                    if key in catalog_keys[standard_key]:
+                    if key in catalog_keys[standard]:
                         control_type = control.parameters[0].text
                     else:
-                        catalog_keys[standard_key][key] = []
+                        catalog_keys[standard][key] = []
                         control_type = control.parameters[0].text + " Not in Profile"
                 else:
-                    control_type = "Shared"
-                catalog_keys[standard_key][key].append(
+                    if key in catalog_keys[standard]:
+                        control_type = "Shared"
+                    else:
+                        catalog_keys[standard][key] = []
+                        control_type = "Shared Not in Profile"
+
+                catalog_keys[standard][key].append(
                             {"system": comp.key,
                              "security_control_type": control_type}
                 )
     return catalog_keys
 
 
-def read_profile_ids(profile_source):
-    url = requests.get(profile_source)
+def read_nist_json_profile_ids(profile_url, quiet):
+    url = requests.get(profile_url)
     profile = json.loads(url.text)
     title = profile["profile"]["metadata"]["title"]
     profile_ids = profile["profile"]["imports"][0]["include-controls"][0]["with-ids"]
-    print("{:d} controls in {:s}".format(len(profile_ids), title))
+    if not quiet:
+        print("{:d} controls in {:s}".format(len(profile_ids), title))
     return profile_ids
 
 
-def print_report(gap_analysis):
+def read_ars_yaml_profile_ids(ars, impact_level, quiet):
+    ars_yml = (
+        'https://raw.githubusercontent.com/CMSgov/ars-machine-readable/main/'
+        '{:s}/generic/{:s}.yml'
+    )
+    profile_url = ars_yml.format(ars, ars)
+    url = requests.get(profile_url)
+    profile = yaml.safe_load(url.text)
+
+    profile_ids = []
+    for control, data in profile.items():
+        if data["Baseline"] and impact_level in data["Baseline"]:
+            profile_ids.append(oscalize_control_id(control))
+    if not quiet:
+        print("{:d} controls in ARS {:s} {:s}".format(len(profile_ids), ars, impact_level))
+    return profile_ids
+
+
+def prepare_report(gap_analysis, quiet):
     rv = {}
     empty = []
     for standard, controls in gap_analysis.items():
-        print("{:d} controls in {:s}".format(len(controls), standard))
+        if not quiet:
+            print("{:d} controls in {:s} (including controls not in Profile)".
+                  format(len(controls), standard))
         for control, types in controls.items():
             if not types:
                 empty.append(control)
@@ -147,58 +173,99 @@ def print_report(gap_analysis):
                             rv[system]["not_hy"].append(control)
                         else:
                             rv[system]["hybrid"].append(control)
-        print("{:<10} {:<10} {:<8} {:<8} {:<8} {:<8}".format(
-            'System', 'Inherited', 'Hybrid', 'In-Not', 'Hy-Not', 'Total'))
-        tot_in = 0
-        tot_hy = 0
-        tot_ni = 0
-        tot_nh = 0
-        totals = 0
-        for system, values in rv.items():
-            inherit = len(rv[system]["inherit"])
-            tot_in += inherit
-            hybrid = len(rv[system]["hybrid"])
-            tot_hy += hybrid
-            not_in = len(rv[system]["not_in"])
-            tot_ni += not_in
-            not_hy = len(rv[system]["not_hy"])
-            tot_nh += not_hy
-            total = inherit + hybrid + not_in + not_hy
-            totals += total
-            print("{:<10} {:<10} {:<8} {:<8} {:<8} {:<8}".format(
-                system, inherit, hybrid, not_in, not_hy, total))
-        print("{:<10} {:<10} {:<8} {:<8} {:<8} {:<8}".format(
-            "Totals", tot_in, tot_hy, tot_ni, tot_nh, totals))
-        print("{:<10} {:<10}".format("Empty:", len(empty)))
+        rv["Empty"] = empty
+    return rv
 
 
-"""
-Return:
-    Count and list of Controls Fully Inherited
-    Count and list of Controls with Shared Responsibility
-    Count and list of Controls not in catalog
-    Count and list of Controls have no implementation statement
-"""
+def print_report(rv):
+    print("{:<10} {:<10} {:<8} {:<8} {:<8} {:<8}".format(
+        'System', 'Inherited', 'Hybrid', 'In-Not', 'Hy-Not', 'Total'))
+    tot_in = 0
+    tot_hy = 0
+    tot_ni = 0
+    tot_nh = 0
+    totals = 0
+    for system, values in rv.items():
+        if system == "Empty":
+            continue
+        inherit = len(rv[system]["inherit"])
+        tot_in += inherit
+        hybrid = len(rv[system]["hybrid"])
+        tot_hy += hybrid
+        not_in = len(rv[system]["not_in"])
+        tot_ni += not_in
+        not_hy = len(rv[system]["not_hy"])
+        tot_nh += not_hy
+        total = inherit + hybrid + not_in + not_hy
+        totals += total
+        print("{:<10} {:<10} {:<8} {:<8} {:<8} {:<8}".format(
+            system, inherit, hybrid, not_in, not_hy, total))
+    print("{:<10} {:<10} {:<8} {:<8} {:<8} {:<8}".format(
+        "Totals", tot_in, tot_hy, tot_ni, tot_nh, totals))
+    print("{:<10} {:<10}".format("Empty:", len(rv["Empty"])))
+
+
+def print_list(rv, shared, inherited, not_in_profile):
+    if inherited:
+        if not_in_profile:
+            column = "not_in"
+        else:
+            column = "inherit"
+    else:
+        if not_in_profile:
+            column = "not_hy"
+        else:
+            column = "hybrid"
+    for system, values in rv.items():
+        if system == "Empty":
+            continue
+        print({system: rv[system][column]})
 
 
 @click.command()
 @click.option(
-    "-e",
-    "--empty",
-    is_flag=True,
-    help="Count and list of controls with no implementation statement",
+    "-a",
+    "--ars",
+    default="3.1",
+    show_default=True,
+    help="ARS machine readable version",
 )
 @click.option(
-    "-h",
-    "--hybrid",
+    "-l",
+    "--level",
+    default="Moderate",
+    show_default=True,
+    help="Impact level: Low, Moderate, High",
+)
+@click.option(
+    "-s",
+    "--shared",
     is_flag=True,
-    help="Count and list of Hybrid or Shared controls",
+    help="List of Shared or Hybrid controls",
 )
 @click.option(
     "-i",
     "--inherited",
     is_flag=True,
-    help="Count and list of Fully Inherited controls",
+    help="List of Fully Inherited controls",
+)
+@click.option(
+    "-n",
+    "--not_in_profile",
+    is_flag=True,
+    help="List controls that are not in profile (with -s or -i)",
+)
+@click.option(
+    "-e",
+    "--empty",
+    is_flag=True,
+    help="List of controls with no implementation statement",
+)
+@click.option(
+    "-q",
+    "--quiet",
+    is_flag=True,
+    help="Output machine-readable lines (with -s, -i or -e)",
 )
 @click.option(
     "-j",
@@ -206,28 +273,34 @@ Return:
     is_flag=True,
     help="Output raw json of catalog control contributions",
 )
-@click.option(
-    "-p",
-    "--profile_url",
-    help="URL to OSCAL Profile in JSON format for tailoring Catalog",
-)
 @click.argument(
     "source",
     type=click.Path(exists=True, dir_okay=False, file_okay=True, resolve_path=True),
 )
-def main(source, profile_url, empty, hybrid, inherited, json_out):
-    if not profile_url:
+def main(source, ars, level, empty, shared, inherited, not_in_profile, json_out, quiet):
+    """
+    Read an OpenControl repo and perform gap analysis against a Catalog Baseline.
+    """
+
+    if ars:
+        profile_ids = read_ars_yaml_profile_ids(ars, level, quiet)
+    else:
         profile = (
             'https://raw.githubusercontent.com/usnistgov/oscal-content/main/nist.gov'
             '/SP800-53/rev4/json/NIST_SP-800-53_rev4_MODERATE-baseline_profile.json'
         )
-    profile_ids = read_profile_ids(profile)
+        profile_ids = read_nist_json_profile_ids(profile, quiet)
     gap_analysis = read_opencontrol_components(source, profile_ids)
     if json_out:
         print(json.dumps(gap_analysis, indent=4))
     else:
-        print_report(gap_analysis)
-    
+        rv = prepare_report(gap_analysis, quiet)
+        if empty:
+            print({"Empty": rv["Empty"]})
+        elif shared or inherited:
+            print_list(rv, shared, inherited, not_in_profile)
+        else:
+            print_report(rv)
 
 
 if __name__ == "__main__":
